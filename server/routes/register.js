91eca25cdda4371d4c95348722b4789c46cadd48
okechukwu promise <okechukwupromise@gmail.com>
2026-08-10 23:52:08 +0100

    fix production api url

--- server/routes/register.js
+++ server/routes/register.js
@@ -60,7 +60,7 @@ router.post("/register", async(req,res) => {
 
     res.cookie("token", token, {
             httpOnly: true,
-            secure: false, // true in production (HTTPS)
+            secure: true, // true in production (HTTPS)
             sameSite: "lax",
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
             });
