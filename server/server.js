5d0747013c606768cbe96a4cbbab3fa486cdc98e
okechukwu promise <okechukwupromise@gmail.com>
2026-08-11 00:23:31 +0100

    fix

--- server/server.js
+++ server/server.js
@@ -1,47 +1,51 @@
 require("dotenv").config();
 
 const express = require("express");
-const cors = require ("cors")
-const dns = require('node:dns');
-dns.setServers(['8.8.8.8', '8.8.4.4']);
-const app = express();
-const dotenv = require ("dotenv")
-const mongoose = require("mongoose")
+const cors = require("cors");
+const dns = require("node:dns");
+const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
-const registerRoutes = require("./routes/register")
-const postRoutes = require("./routes/post")
+const registerRoutes = require("./routes/register");
+const postRoutes = require("./routes/post");
 
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-
-
-
+const app = express();
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
-  .catch(err => console.error("❌ MongoDB connection error:", err.message));
+  .catch((err) =>
+    console.error(
+      "❌ MongoDB connection error:",
+      err.message
+    )
+  );
 
- app.use(express.json());
-  app.use(
+app.use(express.json());
+
+app.use(
   cors({
-    origin: ["http://localhost:5173", 'https://student-connect-eta.vercel.app'],
+    origin: [
+      "http://localhost:5173",
+      "https://student-connect-eta.vercel.app",
+    ],
     credentials: true,
   })
 );
+
 app.use(cookieParser());
 
 app.use("/api/register", registerRoutes);
 app.use("/api/posts", postRoutes);
 
 app.get("/", (req, res) => {
-    res.send("StudyConnect API is running...");
+  res.send("StudyConnect API is running...");
 });
 
+const PORT = process.env.PORT || 7000;
 
-
-const PORT = 7000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
