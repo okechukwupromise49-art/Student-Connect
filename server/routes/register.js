96aa0cf205bd053577826041b5ea72904835a07d
okechukwu promise <okechukwupromise@gmail.com>
2026-08-11 00:04:05 +0100

    fix production authentication

--- server/routes/register.js
+++ server/routes/register.js
@@ -61,7 +61,7 @@ router.post("/register", async(req,res) => {
     res.cookie("token", token, {
             httpOnly: true,
             secure: true, // true in production (HTTPS)
-            sameSite: "lax",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
             });
 
