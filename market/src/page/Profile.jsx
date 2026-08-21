d8c3f4f2fa2222491b810a24ceb816f2e4960f12
okechukwu promise <okechukwupromise@gmail.com>
2026-08-22 00:19:06 +0100

    lenght

--- market/src/page/Profile.jsx
+++ market/src/page/Profile.jsx
@@ -422,7 +422,7 @@ export default function Profile() {
 
               <div className="text-center">
                 <p className="text-xl font-bold text-gray-900">
-                  {user.followers.length || 0}
+                  {user.followers?.length || 0}
                 </p>
 
                 <p className="text-sm text-gray-500">
