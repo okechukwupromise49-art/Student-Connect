36d736707dde2c2c3bb6ea32a993ee3b56a86283
okechukwu promise <okechukwupromise@gmail.com>
2026-08-29 08:13:49 +0100

    v

--- market/src/component/MarketHome.jsx
+++ market/src/component/MarketHome.jsx
@@ -71,7 +71,7 @@ export default function MarketHome() {
 
         // Items
         try {
-          const itemsRes = await axios.get(`${API_URL}/api/market/24`, {
+          const itemsRes = await axios.get(`${API_URL}/api/market/items`, {
             withCredentials: true,
           });
           setItems(itemsRes.data || []);
