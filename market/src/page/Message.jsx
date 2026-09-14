6bbab5f3aec75534690d9a608c95d95679b7cc68
okechukwu promise <okechukwupromise@gmail.com>
2026-09-14 16:58:58 +0100

    update

--- market/src/page/Message.jsx
+++ market/src/page/Message.jsx
@@ -307,8 +307,14 @@ useEffect(() => {
               </div>
                 </div>
 
-                <div className="w-9 h-9 rounded-full bg-indigo-50 flex items-center justify-center shrink-0">
-                  <MessageCircle size={16} className="text-indigo-600" />
+                <div className="relative w-10 h-10 rounded-xl bg-indigo-50 flex items-center justify-center">
+                  <MessageCircle size={20} className="text-indigo-600" />
+
+                  {unreadCount > 0 && (
+                    <span className="absolute -top-1 -right-1 min-w-[20px] h-5 px-1 rounded-full bg-red-500 text-white text-[10px] font-bold flex items-center justify-center">
+                      {unreadCount > 99 ? "99+" : unreadCount}
+                    </span>
+                  )}
                 </div>
               </button>
             ))}
