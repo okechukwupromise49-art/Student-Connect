24c999583ad3d6daee68f4dc227cb805f239fc76
okechukwu promise <okechukwupromise@gmail.com>
2026-09-14 17:06:00 +0100

    unread

--- market/src/page/Message.jsx
+++ market/src/page/Message.jsx
@@ -307,11 +307,11 @@ useEffect(() => {
               </div>
                 </div>
 
-                <div className="relative w-10 h-10 rounded-xl bg-indigo-50 flex items-center justify-center">
+                <div className={`relative w-10 h-10 rounded-xl bg-indigo-50 flex items-center justify-center ${unreadCount > 0 && "bg-green-100"}`}>
                   <MessageCircle size={20} className="text-indigo-600" />
 
-                  {unreadCount > 0 && (
-                    <span className="absolute -top-1 -right-1 min-w-[20px] h-5 px-1 rounded-full bg-red-500 text-white text-[10px] font-bold flex items-center justify-center">
+                  { unreadCount > 0 &&(
+                    <span className="absolute -top-1 -right-1 min-w-[20px] h-5 px-1 rounded-full bg-green-500 text-white text-[10px] font-bold flex items-center justify-center">
                       {unreadCount > 99 ? "99+" : unreadCount}
                     </span>
                   )}
